4c19679ffe64d8f728921611e8826bde9f602614
matthew.mitchell <matthew.mitchell@mu-c1-07.mullen.pvt.k12.co.us>
2018-10-11 14:57:55 -0600

    initial commit

--- BAttleSHip/src/battleshipRunner.java
+++ BAttleSHip/src/battleshipRunner.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class battleshipRunner
 	{
@@ -5,7 +6,39 @@ public class battleshipRunner
 		public static void main(String[] args)
 			{
 				Layout.display();
-
+				greet();
 			}
 
+		public static void greet()
+			{
+			System.out.println("What difficultly would you like?");
+			Scanner userInput = new Scanner(System.in);
+			String difficulty = userInput.nextLine();
+			if (difficulty.equals("easy"))
+				{
+					easy();
+				}
+			else if (difficulty.equals("hard"))
+				{
+					System.out.println("You have chosen hard mode");
+				}
+			else
+				{
+					System.out.println("Choose easy or hard.");
+					greet();
+				}
+			}
+		public static void easy()
+			{
+			System.out.println("pick spots where you would like to place your ships");
+			System.out.println("each ship takes one space (e.g. A1)");
+			Scanner userInput = new Scanner(System.in);
+			String rowImput = userInput.nextLine();
+			rowImput = move.substring(0,1);
+			rowImput = rowImput.toUpperCase();
+			if(rowImput.equals("A1"))
+				{
+					
+				}
+			}
 	}
