42fc4ba8864819a700b1e93c463ece08ef3cd9b1
matthew.mitchell <matthew.mitchell@mu-c1-07.mullen.pvt.k12.co.us>
2018-10-12 14:59:33 -0600

    add layout assignments

--- BAttleSHip/src/Layout.java
+++ BAttleSHip/src/Layout.java
@@ -6,10 +6,10 @@ public class Layout
 		public static void createLayout()
 			{
 				
-				Layout = new String [6][6];
-				for(int i = 0; i < 6; i++)
+				Layout = new String [5][5];
+				for(int i = 0; i < 5; i++)
 					{
-						for(int j = 0; j < 6; j++)
+						for(int j = 0; j < 5; j++)
 							{
 								Layout [i][j] = "  ";
 							}
@@ -23,19 +23,19 @@ public class Layout
 			System.out.println("|       |   A   |   B   |   C   |   D   |   E   |");
 			System.out.println("|_______|_______|_______|_______|_______|_______|");
 			System.out.println("|       |       |       |       |       |       |");
-			System.out.println("|   1   |       |       |       |       |       |");
+			System.out.println("|   1   | "  + Layout[0][0] +  "	| "  + Layout[0][1] +  "	| "  + Layout[0][2] +  "	| "  + Layout[0][3] +  "    |   "  + Layout[0][4] +  "  | ");
 			System.out.println("|_______|_______|_______|_______|_______|_______|");
 			System.out.println("|       |       |       |       |       |       |");
-			System.out.println("|   2   |       |       |       |       |       |");
+			System.out.println("|   2   | "  + Layout[1][0] +  "	| "  + Layout[1][1] +  "	| "  + Layout[1][2] +  "	| "  + Layout[1][3] +  "    |   "  + Layout[1][4] +  "  | ");
 			System.out.println("|_______|_______|_______|_______|_______|_______|");
 			System.out.println("|       |       |       |       |       |       |");
-			System.out.println("|   3   |       |       |       |       |       |");
+			System.out.println("|   3   | "  + Layout[2][0] +  "	| "  + Layout[2][1] +  "	| "  + Layout[2][2] +  "	| "  + Layout[2][3] +  "    |   "  + Layout[2][4] +  "  | ");
 			System.out.println("|_______|_______|_______|_______|_______|_______|");
 			System.out.println("|       |       |       |       |       |       |");
-			System.out.println("|   4   |       |       |       |       |       |");
+			System.out.println("|   4   | "  + Layout[3][0] +  "	| "  + Layout[3][1] +  "	| "  + Layout[3][2] +  "	| "  + Layout[3][3] +  "    |   "  + Layout[3][4] +  "  | ");
 			System.out.println("|_______|_______|_______|_______|_______|_______|");
 			System.out.println("|       |       |       |       |       |       |");
-			System.out.println("|   5   |       |       |       |       |       |");			                                      
+			System.out.println("|   5   | "  + Layout[4][0] +  "	| "  + Layout[4][1] +  "	| "  + Layout[4][2] +  "	| "  + Layout[4][3] +  "    |   "  + Layout[4][4] +  "  | ");			                                      
 			System.out.println("|_______|_______|_______|_______|_______|_______|");
 
 			}
--- BAttleSHip/src/battleshipRunner.java
+++ BAttleSHip/src/battleshipRunner.java
@@ -5,6 +5,7 @@ public class battleshipRunner
 
 		public static void main(String[] args)
 			{
+				Layout.createLayout();
 				Layout.display();
 				greet();
 			}
@@ -34,11 +35,27 @@ public class battleshipRunner
 			System.out.println("each ship takes one space (e.g. A1)");
 			Scanner userInput = new Scanner(System.in);
 			String rowImput = userInput.nextLine();
-			rowImput = move.substring(0,1);
+			//rowImput = move.substring(0,1);
 			rowImput = rowImput.toUpperCase();
 			if(rowImput.equals("A1"))
 				{
-					
+					//assign it to A1
+				}
+			else if (rowImput.equals("A2"))
+				{
+					//assign to A2
+				}
+			else if (rowImput.equals("A3"))
+				{
+					//assign to A3
+				}
+			else if (rowImput.equals("A4"))
+				{
+					//assign to A4
+				}
+			else if (rowImput.equals("A5"))
+				{
+					//assign to A5
 				}
 			}
 	}
